e6c3d5482d5ba6ba630655d224eeb110fd655a14
Vanshay Gupta <52407751+VanshayGupta@users.noreply.github.com>
2020-11-08 10:00:19 +0530

    changes to lock

--- index.php
+++ index.php
@@ -3,17 +3,16 @@ include('db.php');
 ?>
 <?php ob_start() ?>
 <?php session_start() ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <title>Sun of Beach Hotels</title>
-<!-- for-mobile-apps -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Resort Inn Responsive , Smartphone Compatible web template , Samsung, LG, Sony Ericsson, Motorola web design" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
 		function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!-- //for-mobile-apps -->
 <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
 <link href="css/font-awesome.css" rel="stylesheet"> 
 <link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen">
@@ -22,11 +21,11 @@ include('db.php');
 <link rel="stylesheet" href="css/jquery-ui.css" />
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
 <script type="text/javascript" src="js/modernizr-2.6.2.min.js"></script>
-<!--fonts-->
+
 <link href="//fonts.googleapis.com/css?family=Oswald:300,400,700" rel="stylesheet">
 <link href="//fonts.googleapis.com/css?family=Federo" rel="stylesheet">
 <link href="//fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
-<!--//fonts-->
+
 <link rel="icon" href="images/icon.jpg">
 </head>
 <body>
@@ -610,10 +609,7 @@ fit: true
 });
 });
 </script>
-<!--//tabs-->
-<!-- smooth scrolling -->
-	
-<!-- //smooth scrolling -->
+
 <script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
 </body>
 </html>
